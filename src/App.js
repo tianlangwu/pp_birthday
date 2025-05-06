623b9542d4c666d436fc6d45d28eba291a33809c
Junyong <anxindanding0306@gmail.com>
2025-05-06 15:08:54 -0400

    fix a bug in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BirthdayWheel from "./BirthdayWheel";
 import BirthdayWelcome from "./BirthdayWelcome";
 
